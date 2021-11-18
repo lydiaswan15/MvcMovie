71d817a19c37e679b05d62e5cb17c62671789a27
lydiaswan15 <lydiaswan15@gmail.com>
2021-11-17 19:38:01 -0700

    Getting the basic requirements done.

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -14,23 +14,34 @@ namespace MvcMovie
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
 
             services.AddDbContext<MvcMovieContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("MvcMovieContext")));
+            {
+                var connectionString = Configuration.GetConnectionString("MvcMovieContext");
+
+                if (Environment.IsDevelopment())
+                {
+                    options.UseSqlite(connectionString);
+                }
+                else
+                {
+                    options.UseSqlServer(connectionString);
+                }
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -40,7 +51,6 @@ namespace MvcMovie
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
